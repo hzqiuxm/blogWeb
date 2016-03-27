e453efd240c13a1464deaa2ee655b50dcbfa804f
hzqiuxm <qiuxiaomin@yunhetong.net>
2016-03-27 14:39:06 +0800

    add lucene module

--- service/src/main/java/com/blogweb/search/Indexer.java
+++ service/src/main/java/com/blogweb/search/Indexer.java
@@ -2,8 +2,7 @@ package com.blogweb.search;
 
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
-import org.apache.lucene.document.Document;
-import org.apache.lucene.document.Field;
+import org.apache.lucene.document.*;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.store.Directory;
@@ -12,6 +11,7 @@ import org.apache.lucene.store.FSDirectory;
 import java.io.File;
 import java.io.FileFilter;
 import java.io.FileReader;
+import java.nio.file.Files;
 import java.nio.file.Paths;
 
 /**
@@ -19,9 +19,18 @@ import java.nio.file.Paths;
  * 生成索引
  */
 public class Indexer {
+    private IndexWriter writer;
+    public static boolean isCreate = true;
+    final static String indexDir = "D://lucene//test1";
+    final static String dataDir = "D://lucene//test2";
+
     public static void main(String[] args) throws Exception{
-        final String indexDir = "D://lucene//test1";
-        final String dataDir = "D://lucene//test2";
+        for(int i=0; i<args.length; i++){
+            if("-update".equals(args[i])){
+                isCreate = false;
+                break;
+            }
+        }
 
         long startTime  = System.currentTimeMillis();
         Indexer indexer = new Indexer(indexDir);
@@ -36,8 +45,6 @@ public class Indexer {
 
 }
 
-    private IndexWriter writer;
-
     public Indexer(String indexDir) throws Exception{
 
         //存放索引的地方
@@ -45,6 +52,16 @@ public class Indexer {
         Directory dir = FSDirectory.open(Paths.get(indexDir));
         Analyzer analyzer  = new StandardAnalyzer();
         IndexWriterConfig iwc = new IndexWriterConfig(analyzer);
+        if(isCreate){
+            iwc.setOpenMode(IndexWriterConfig.OpenMode.CREATE);
+        }else {
+            iwc.setOpenMode(IndexWriterConfig.OpenMode.CREATE_OR_APPEND);
+        }
+        if(!Files.isWritable(Paths.get(indexDir))){
+            System.out.println("存放索引文件目录不可写");
+        }else{
+            System.out.println("indexDir 目录可写!");
+        }
         writer = new IndexWriter(dir,iwc);
 
     }
@@ -70,12 +87,30 @@ public class Indexer {
             return pathname.getName().toLowerCase().endsWith(".txt");
         }
     }
+
     protected Document getDocument(File file) throws Exception{
         Document doc = new Document();
-        doc.add(new Field("wendnag",new FileReader(file)));
-        doc.add(new Field("wenjianming", file.getName(),Field.Store.YES,Field.Index.NOT_ANALYZED));
-        doc.add(new Field("lujing", file.getCanonicalPath(),Field.Store.YES,Field.Index.NOT_ANALYZED));
+       /* //老版本写法
+        doc.add(new Field("文档",new FileReader(file)));
+        doc.add(new Field("文件名", file.getName(),Field.Store.YES,Field.Index.NOT_ANALYZED));
+        doc.add(new Field("路径", file.getCanonicalPath(),Field.Store.YES,Field.Index.NOT_ANALYZED));*/
 
+        //新版本写法
+        //NIO的写法需要传入docDir必须是文件
+//        final Path docDir = Paths.get("D://lucene//test2//新建文本文档.txt");
+//        InputStream stream = Files.newInputStream(docDir, StandardOpenOption.CREATE);
+        Field pathField = new StringField("文档", file.toString(), Field.Store.YES);
+        Field fileName = new StringField("文件名",file.getName(),Field.Store.YES);
+        Field filePath = new StringField("路径",file.getCanonicalPath(),Field.Store.YES);
+        Field modfied = new LongField("modified",Files.getLastModifiedTime(Paths.get(dataDir)).toMillis(),Field.Store.YES);
+
+        doc.add(pathField);
+        doc.add(fileName);
+        doc.add(filePath);
+        doc.add(modfied);
+        //对应NIO写法
+//        doc.add(new TextField("内容",new BufferedReader(new InputStreamReader(stream, StandardCharsets.UTF_8))));
+        doc.add(new TextField("contents",new FileReader(file)));
         return doc;
     }
 
